a48ac83d4e21603a03749e0667af2c0495820cc7
Louis <louis@Louiss-MacM1Book-Pro.local>
2023-11-18 20:57:50 +0800

    Rename function

--- src/app/click/click.component.ts
+++ src/app/click/click.component.ts
@@ -11,7 +11,7 @@ export class ClickComponent {
   showInputRightClick: boolean = false;
   inputValue: string = '';
 
-  togglePopover() {
+  onSingleClick() {
     this.showHello = !this.showHello;
   }
 
